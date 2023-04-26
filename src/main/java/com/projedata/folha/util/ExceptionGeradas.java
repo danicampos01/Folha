c57e7f73da818315a7f2fed1be183652614d6b8e
Daniel Campos <danicampos01@gmail.com>
2023-04-26 11:52:25 -0300

    data sjson carregada para implentacao futura de impotacao de arquivo json .

---
+++ src/main/java/com/projedata/folha/util/ExceptionGeradas.java
@@ -0,0 +1,12 @@
+package com.projedata.folha.util;
+
+public class ExceptionGeradas extends Exception {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public ExceptionGeradas(String message) {
+		super(message);
+	}
+
+}
